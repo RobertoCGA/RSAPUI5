43c5e7dcc1ac5004335eb17941e44e5c546d5d02
Roberto Gallardo <robcga3@gmail.com>
2026-03-20 13:07:51 +0000

    finished course

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -10,7 +10,16 @@ sap.ui.define([
 
         return Controller.extend("sap.training.exc.controller.Detail", {
 
-        onNavBack: function () {
+            onInit: function () {
+                var oRouter = this.getOwnerComponent().getRouter();
+                oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+            },
+
+            _onObjectMatched: function (oEvent) {
+                this.getView().bindElement("/UX_Customer" + oEvent.getParameter("arguments").customerId);
+            },
+
+            onNavBack: function () {
             var oHistory = History.getInstance();
             var sPreviousHash = oHistory.getPreviousHash();
 
@@ -21,6 +30,6 @@ sap.ui.define([
                 oRouter.navTo("overview", {}, true);
             }
         }
-
+       
         });
     });
